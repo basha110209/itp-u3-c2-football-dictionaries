1154e1df32a7f5623df9f7e5ea6440505e5e87c8
basha110209 <bashayr1029@gmail.com>
2024-10-24 01:55:25 +0300

    Update assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
     pass
+    
+def lists_to_dicts(players):
+    keys = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    return [dict(zip(keys, player)) for player in squads_list]
+
+# Example usage
+SQUADS_DATA = [
+    ["1", "GK", "Juan Botasso", "(1908-10-23)23 October 1908 (aged 21)", "", "Quilmes", "Argentina", "Argentina", "1930"],
+    ["9", "FW", "Roberto Cherro", "(1907-02-23)23 February 1907 (aged 23)", "", "Boca Juniors", "Argentina", "Argentina", "1930"]
+]
+
+players_dicts = players_as_dictionaries(SQUADS_DATA)
+print(players_dicts)
